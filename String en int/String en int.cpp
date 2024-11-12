faaa797b5a598339c6e781d72c0daf4b071ab657
fraigneau <fraigneau86@yahoo.fr>
2024-11-12 14:20:29 +0100

    refactor

--- String en int/String en int.cpp
+++ String en int/String en int.cpp
@@ -2,27 +2,20 @@
 #include <string>
 #include <bitset>
 
-std::string CharEnBinaire(char c) {
-    std::string binaire;
-    for (int i = 3; i >= 0; i--) {
-        binaire += ((c >> i) & 1) ? '1' : '0';
-    }
-    return binaire;
-}
-
-int BinaireEnInt(std::string str) {
-    std::bitset<4> bits(str);
-    return static_cast<int>(bits.to_ulong());
-}
-
 int main() {
-    char input[] = "123546";
 	int x = 0;
-
+	char input[] = "123";
+	if (strlen(input) == 0 && strlen(input) > 6) { return 1; }
     for (int i = 0; i < strlen(input); i++) {
-		int y = BinaireEnInt(CharEnBinaire(input[i])); // recup 
-		x = x * 10 + y;
-	}
+		if ((input[i] & 0x30) != 0x30) {
+			std::cout << "Le caractere " << input[i] << " n'est pas un chiffre" << std::endl;
+			return 2;
+		}
+		else {
+			int y = (input[i] & 0x0F);
+			x = x * 10 + y;
+		}
+    }
 	std::cout << "Le nombre entier correspondant est : " << x << std::endl;
     return 0;
 }
